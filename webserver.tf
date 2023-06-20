ab29e6eb35523f1a81467f3ea7124cebe2f2d140
Marcus Ross <marcus.ross@zahlenhelfer.de>
2023-06-20 12:15:40 +0200

    change ec2 to custom vpc usage

--- webserver.tf
+++ webserver.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "web" {
-  ami             = "ami-0b2ac948e23c57071"
-  instance_type   = "t3.micro"
-  user_data       = file("install_webserver.sh")
-  security_groups = [aws_security_group.allow_http.name]
+  ami                    = "ami-0b2ac948e23c57071"
+  instance_type          = "t3.micro"
+  user_data              = file("install_webserver.sh")
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  subnet_id              = module.vpc.public_subnets[0]
   tags = {
     Name       = "HelloWorld"
     Costcenter = "666"
@@ -12,9 +13,9 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow web inbound traffic"
-
+  vpc_id      = module.vpc.vpc_id
   ingress {
-    description = "TLS from VPC"
+    description = "Web from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
